9007c2ed4b5fbf6353d4c04fdd0445bbff20da05
Philipp Memmel <philipp.memmel@gitlab.mebis.alp.dillingen.de>
2021-10-14 15:48:11 +0200

    added javadoc to ServerMessage

--- src/ServerMessage.java
+++ src/ServerMessage.java
@@ -8,6 +8,12 @@ public class ServerMessage implements Serializable {
   private String content;
   private String from;
 
+  /**
+   * Message being sent from the server to the clients, including additional information.
+   *
+   * @param content the content of the message
+   * @param from the source client's name
+   */
   public ServerMessage(String content, String from) {
     this.content = content;
     this.from = from;
